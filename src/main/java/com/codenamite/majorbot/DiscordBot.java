a524a6b42d14e4da4c1edbe7dae7195a63db6e50
Codenamite <codenamite@gmail.com>
2021-05-17 21:29:34 +0200

    Refactored DiscordBot - beans are injected through constructor now

--- src/main/java/com/codenamite/majorbot/DiscordBot.java
+++ src/main/java/com/codenamite/majorbot/DiscordBot.java
@@ -7,34 +7,44 @@ import discord4j.core.GatewayDiscordClient;
 import io.micronaut.context.annotation.Value;
 import io.micronaut.runtime.event.annotation.EventListener;
 import io.micronaut.runtime.server.event.ServerStartupEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.IOException;
 
 @Singleton
 class DiscordBot {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiscordBot.class);
+
     @Value("${api.discord.key}")
     private String keyId;
 
-    @Inject
-    private DiscordEvents discordEvents;
-    @Inject
-    DiscordCommands discordCommands;
+    public DiscordBot(DiscordEvents discordEvents, DiscordCommands discordCommands) {
+        this.discordEvents = discordEvents;
+        this.discordCommands = discordCommands;
+    }
+
+    private final DiscordEvents discordEvents;
+    private final DiscordCommands discordCommands;
 
     @EventListener
     void onApplicationEvent(ServerStartupEvent event) throws IOException {
-
         GatewayDiscordClient client =
                 DiscordClientBuilder.create(keyId)
                         .build()
                         .login()
                         .block();
         assert client != null;
+        LOGGER.info("Discord client started");
         discordEvents.createEvents(client);
+        LOGGER.info("Events created");
         discordCommands.putCommandsIntoRegistry();
+        LOGGER.info("Commands pushed to registry");
         discordCommands.registerCommands(client);
+        LOGGER.info("Commands registered");
         client.onDisconnect().block();
+        LOGGER.info("Ready to rumble!");
     }
 }
